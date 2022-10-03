4e548370ae0ae2f52cc6d384385656c7ac410652
Gaurav Kumar <gauravksh1609@gmail.com>
2022-10-03 17:01:40 +0530

    bubble sort

---
+++ C++/bubble-sort.cpp
@@ -0,0 +1,24 @@
+// Bubble sort in C++
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+   int i, j,temp,pass=0;
+   int a[10] = {10,2,0,14,43,25,18,1,5,45};
+   for(i = 0; i < 10; i++) {
+      for(j = i+1; j < 10; j++) {
+         if(a[j] < a[i]) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+         }
+      }
+   }
+   cout <<"Sorted Element List ...\n";
+   for(i = 0; i<10; i++) {
+      cout <<a[i]<<"\t";
+   }
+   return 0;
+}
