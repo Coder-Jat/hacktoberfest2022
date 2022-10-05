6eb6b34a3f0fa80185c2b8806c849b4852f74f66
Kritika Tripathi <95476156+KritikaTri@users.noreply.github.com>
2022-10-05 12:03:53 +0530

    Update binary-search.cpp

--- C++/binary-search.cpp
+++ C++/binary-search.cpp
@@ -30,11 +30,15 @@ int binarySearch(int arr[], int l, int r, int x)
 	return -1;
 }
 
-int main(void)
+int main()
 {
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 10;
-	int n = sizeof(arr) / sizeof(arr[0]);
+	int n,x;
+	cin>>n;    //Input array size               
+	int arr[n];
+	for(int i=0;i<n;i++){  //Input the array
+            cin>>arr[i];
+	}
+	cin>>x;  //Input element to be searched
 	int result = binarySearch(arr, 0, n - 1, x);
 	(result == -1)
 		? cout << "Element is not present in array"
